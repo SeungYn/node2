07837ff0d2c0dcf92c3f18fa9fc58bd991ad1119
SeungYn <kwls0407@gmail.com>
2021-09-12 00:48:01 +0900

    node server

--- node-server/1-http.js
+++ node-server/1-http.js
@@ -1,12 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 //const http2 = require('http2') https
 
 const server = http.createServer((req, res, next) => {
   console.log('incoming...');
   console.log(req.headers);
   console.log(req.method);
-  res.write('welcome');
-  res.end();
+
+  const url = req.url;
+  if (url === '/') {
+    fs.createReadStream('./html/index.html').pipe(res);
+  } else if (url === '/courses') {
+    fs.createReadStream('./html/courses.html').pipe(res);
+  } else {
+    fs.createReadStream('./html/not-found.html').pipe(res);
+  }
 });
 
 server.listen(8080);
